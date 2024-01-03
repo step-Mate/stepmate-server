a9e602e70ccb85426066ee8a528585bc21098880
apfhd12gk <apfhd12gk@naver.com>
2024-01-03 21:19:09 +0900

    Spring Initializer

---
+++ src/main/java/server/stepmate/StepMateApplication.java
@@ -0,0 +1,13 @@
+package server.stepmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepMateApplication.class, args);
+	}
+
+}
