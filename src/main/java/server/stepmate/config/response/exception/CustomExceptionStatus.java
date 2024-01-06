0db1fd0c76f9f5f975658c537901a4080fac99ae
apfhd12gk <apfhd12gk@naver.com>
2024-01-06 15:55:42 +0900

    feat: 400 Request오류 추가

--- src/main/java/server/stepmate/config/response/exception/CustomExceptionStatus.java
+++ src/main/java/server/stepmate/config/response/exception/CustomExceptionStatus.java
@@ -10,7 +10,9 @@ public enum CustomExceptionStatus {
     /**
      * 400 : Request 오류
      */
-    REQUEST_ERROR(false, 400, "입력값을 확인해주세요.");
+    REQUEST_ERROR(false, 400, "입력값을 확인해주세요."),
+    EMPTY_JWT(false, 401, "JWT이 비어있습니다."),
+    INVALID_JWT(false, 402, "유효하지 않은 JWT입니다.");
 
 
     private final boolean isSuccess;
