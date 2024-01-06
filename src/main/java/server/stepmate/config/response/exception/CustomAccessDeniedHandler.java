46e192afa6f2f0f5dadd3f4c0f065ffcbdd5d5c6
apfhd12gk <apfhd12gk@naver.com>
2024-01-06 15:42:55 +0900

    feat: AccessDeniedHandler 인터페이스 구현체 구현

---
+++ src/main/java/server/stepmate/config/response/exception/CustomAccessDeniedHandler.java
@@ -0,0 +1,26 @@
+package server.stepmate.config.response.exception;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+
+        log.warn(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"))+" : Exception transmitted to AccessDeniedHandler");
+
+        response.sendRedirect("/error/access-Denied");
+
+    }
+}
