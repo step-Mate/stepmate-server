1f7e8c86855769de99e6bf3a9841f43bed7b7808
apfhd12gk <apfhd12gk@naver.com>
2024-01-09 09:15:54 +0900

    feat: UserDetailService, UserDetailService 구현

---
+++ src/main/java/server/stepmate/config/security/authentication/CustomUserDetailService.java
@@ -0,0 +1,26 @@
+package server.stepmate.config.security.authentication;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import server.stepmate.user.UserRepository;
+import server.stepmate.user.entity.User;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+
+        Optional<User> optional = userRepository.findByUserId(userId);
+        if(optional.isEmpty()) return null;
+        else return new CustomUserDetails(optional.get());
+    }
+}
