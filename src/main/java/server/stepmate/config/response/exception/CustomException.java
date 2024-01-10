c98d5efed3fb4d0701c7e7b68aa6a599d8d4d8f2
apfhd12gk <apfhd12gk@naver.com>
2024-01-10 09:13:33 +0900

    feat: CustomException에 RuntimeException을 상속
    예외를 언체크 예외로 사용하기 위함

--- src/main/java/server/stepmate/config/response/exception/CustomException.java
+++ src/main/java/server/stepmate/config/response/exception/CustomException.java
@@ -5,6 +5,6 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class CustomException {
+public class CustomException extends RuntimeException{
     CustomExceptionStatus customExceptionStatus;
 }
