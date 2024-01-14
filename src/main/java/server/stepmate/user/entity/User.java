e8d6374beb5614e1ab72b713a694f410c7be9694
apfhd12gk <apfhd12gk@naver.com>
2024-01-14 21:08:02 +0900

    feat:User UserAuthDto 변환 메소드 생성

--- src/main/java/server/stepmate/user/entity/User.java
+++ src/main/java/server/stepmate/user/entity/User.java
@@ -5,7 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import server.stepmate.user.dto.UserAuthDto;
 import server.stepmate.user.entity.enumtypes.RoleType;
+
+import static server.stepmate.user.entity.enumtypes.RoleType.*;
 
 @Getter
 @Builder
@@ -37,5 +40,29 @@ public class User {
     @Enumerated(EnumType.STRING)
     private RoleType role;
 
+    public static User createUser(UserAuthDto dto) {
+        return User.builder()
+                .userId(dto.getUserId())
+                .password(dto.getPassword())
+                .nickname(dto.getNickname())
+                .email(dto.getEmail())
+                .age(dto.getAge())
+                .height(dto.getHeight())
+                .weight(dto.getWeight())
+                .role(ROLE_USER)
+                .build();
+    }
+
+    public UserAuthDto getUserAuthDto() {
+        return UserAuthDto.builder()
+                .id(this.id)
+                .userId(this.userId)
+                .nickname(this.nickname)
+                .email(this.email)
+                .age(this.age)
+                .height(this.height)
+                .weight(this.weight)
+                .build();
+    }
 
 }
