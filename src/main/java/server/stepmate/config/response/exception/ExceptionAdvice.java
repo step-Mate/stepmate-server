ff9ac6118ad48e556b30f5471066311e63a35a0a
apfhd12gk <apfhd12gk@naver.com>
2024-01-15 23:08:50 +0900

    feat: ExceptionAdvice 예외처리 핸들러 생성

---
+++ src/main/java/server/stepmate/config/response/exception/ExceptionAdvice.java
@@ -0,0 +1,29 @@
+package server.stepmate.config.response.exception;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import server.stepmate.config.response.CommonResponse;
+import server.stepmate.config.response.ResponseService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    private final ResponseService responseService;
+
+    @ExceptionHandler(CustomException.class)
+    @ResponseStatus(HttpStatus.OK)
+    protected CommonResponse customException(CustomException customException) {
+        CustomExceptionStatus status = customException.getCustomExceptionStatus();
+        log.warn(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + " : " + status.getMessage());
+        return responseService.getExceptionResponse(status);
+    }
+}
