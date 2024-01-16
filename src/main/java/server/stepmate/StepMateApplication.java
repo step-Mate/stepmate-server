3535344756141887c70e3f6fef667fbe9813a2e2
apfhd12gk <apfhd12gk@naver.com>
2024-01-16 21:11:29 +0900

    feat: Redis 설정

--- src/main/java/server/stepmate/StepMateApplication.java
+++ src/main/java/server/stepmate/StepMateApplication.java
@@ -2,8 +2,10 @@ package server.stepmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class StepMateApplication {
 
 	public static void main(String[] args) {
