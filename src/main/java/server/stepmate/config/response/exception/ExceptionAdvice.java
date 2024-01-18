8962f38ff25e7dbeca85e33d4cd9ce6bcb5abecf
apfhd12gk <apfhd12gk@naver.com>
2024-01-18 10:43:54 +0900

    feat: 오류별로 상태코드를 변경하도록 수정

--- src/main/java/server/stepmate/config/response/exception/ExceptionAdvice.java
+++ src/main/java/server/stepmate/config/response/exception/ExceptionAdvice.java
@@ -3,6 +3,7 @@ package server.stepmate.config.response.exception;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -20,10 +21,9 @@ public class ExceptionAdvice {
     private final ResponseService responseService;
 
     @ExceptionHandler(CustomException.class)
-    @ResponseStatus(HttpStatus.OK)
-    protected CommonResponse customException(CustomException customException) {
+    protected ResponseEntity<CommonResponse> customException(CustomException customException) {
         CustomExceptionStatus status = customException.getCustomExceptionStatus();
         log.warn(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + " : " + status.getMessage());
-        return responseService.getExceptionResponse(status);
+        return ResponseEntity.status(status.getCode()).body(responseService.getExceptionResponse(status));
     }
 }
