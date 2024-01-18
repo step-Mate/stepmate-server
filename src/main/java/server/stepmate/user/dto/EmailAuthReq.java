68183f466ab747de131bfb51adb011e0bb8eb7f3
apfhd12gk <apfhd12gk@naver.com>
2024-01-18 11:34:07 +0900

    feat: 이메일 인증 dto 생성

---
+++ src/main/java/server/stepmate/user/dto/EmailAuthReq.java
@@ -0,0 +1,19 @@
+package server.stepmate.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class EmailAuthReq {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    private String authCode;
+}
