c9d9de922345bf3dcf46c1043ecb5849b6e367ab
apfhd12gk <apfhd12gk@naver.com>
2024-01-21 21:26:38 +0900

    feat: 유저미션 저장소에서 진행중인 미션 5개 추출

--- src/main/java/server/stepmate/mission/UserMissionRepository.java
+++ src/main/java/server/stepmate/mission/UserMissionRepository.java
@@ -1,7 +1,14 @@
 package server.stepmate.mission;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import server.stepmate.mission.entity.UserMission;
 
+import java.util.List;
+
 public interface UserMissionRepository extends JpaRepository<UserMission,Long> {
+
+    @Query("SELECT i from UserMission i WHERE i.user.id = :id and i.isComplete = false order by i.mission.id asc ")
+    List<UserMission> findTop5ByUserMission(@Param("id") Long id);
 }
