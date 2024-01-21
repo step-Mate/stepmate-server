32ade5b48a1e13ec0e12b49e3d586664b69d9e0b
apfhd12gk <apfhd12gk@naver.com>
2024-01-21 18:45:05 +0900

    feat: AccessToken Dto 생성

---
+++ src/main/java/server/stepmate/user/dto/AccessTokenDto.java
@@ -0,0 +1,11 @@
+package server.stepmate.user.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccessTokenDto {
+    private String accessToken;
+}
