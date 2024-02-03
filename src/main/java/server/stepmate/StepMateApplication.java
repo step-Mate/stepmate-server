5dd399428d1bf195298b2aba3e8907dd04f79530
apfhd12gk <apfhd12gk@naver.com>
2024-02-03 22:54:59 +0900

    feat: EC2 서버에서 스프링프로젝트 시간을 Asia/Seoul로 변경

--- src/main/java/server/stepmate/StepMateApplication.java
+++ src/main/java/server/stepmate/StepMateApplication.java
@@ -1,15 +1,22 @@
 package server.stepmate;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
 public class StepMateApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(StepMateApplication.class, args);
 	}
