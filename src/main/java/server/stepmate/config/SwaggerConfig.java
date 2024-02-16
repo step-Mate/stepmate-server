21256dd909039b4502034d3f5a7e6be5a11b1eff
apfhd12gk <apfhd12gk@naver.com>
2024-02-16 20:47:26 +0900

    feat: API swagger 문서 Security 기능 추가

--- src/main/java/server/stepmate/config/SwaggerConfig.java
+++ src/main/java/server/stepmate/config/SwaggerConfig.java
@@ -1,8 +1,10 @@
 package server.stepmate.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +12,16 @@ import org.springframework.context.annotation.Configuration;
         info = @Info(title = "step-mate",
         description = "step-mate App Back-End 연동 문서",
         version = "v1"),
+        security = @SecurityRequirement(name = "JWT"),
         servers = {
         @Server(url = "/", description = "Default Server url")
 }
+)
+@SecurityScheme(
+        name = "JWT",
+        type = SecuritySchemeType.APIKEY,
+        in = SecuritySchemeIn.HEADER,
+        paramName = "Authorization"
 )
 @Configuration
 public class SwaggerConfig {
