cdc7ba95ef5db21ad22e1da256e244f1fce58399
apfhd12gk <apfhd12gk@naver.com>
2024-03-11 20:20:04 +0900

    feat: 유저 조회시 유저 진행중인 미션을 타입별로 하나씩 보여주도록 변경

--- src/main/java/server/stepmate/mission/MissionService.java
+++ src/main/java/server/stepmate/mission/MissionService.java
@@ -7,14 +7,17 @@ import org.springframework.transaction.annotation.Transactional;
 import server.stepmate.config.response.exception.CustomException;
 import server.stepmate.config.response.exception.CustomExceptionStatus;
 import server.stepmate.config.security.authentication.CustomUserDetails;
+import server.stepmate.mission.dto.MissionDetailDto;
 import server.stepmate.mission.dto.MissionDto;
 import server.stepmate.mission.dto.MissionProgressDto;
 import server.stepmate.mission.dto.MissionDesignationDto;
 import server.stepmate.mission.entity.Mission;
 import server.stepmate.mission.entity.UserMission;
+import server.stepmate.mission.entity.enumtypes.MissionType;
 import server.stepmate.user.UserRepository;
 import server.stepmate.user.entity.User;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,9 +34,46 @@ public class MissionService {
 
     public List<MissionDto> getMissions(CustomUserDetails customUserDetails) {
         User user = customUserDetails.getUser();
-        List<UserMission> userMissions = userMissionRepository.findAllById(user.getId());
 
-        return getMissionDtoList(userMissions);
+        List<MissionDto> missionDtoList = new ArrayList<>();
+
+        List<UserMission> userMissions = userMissionRepository.findAllNoneMissionById(user.getId());
+        List<UserMission> weeklyStepMission = userMissionRepository.findAllWeeklyStepMissionById(user.getId());
+        List<UserMission> weeklyCalorieMission = userMissionRepository.findAllWeeklyCalorieMissionById(user.getId());
+
+        for (int i = 0; i < weeklyStepMission.size(); i++) {
+
+            List<MissionDetailDto> missionDetailDtoList = new ArrayList<>();
+
+            MissionDetailDto stepBuild = MissionDetailDto.builder()
+                    .missionType(MissionType.STEP)
+                    .currentValue(weeklyStepMission.get(i).getStepCurrentValue())
+                    .goal(weeklyStepMission.get(i).getMission().getGoal())
+                    .build();
+
+            MissionDetailDto calorieBuild = MissionDetailDto.builder()
+                    .missionType(MissionType.CALORIE)
+                    .currentValue(weeklyCalorieMission.get(i).getCalorieCurrentValue())
+                    .goal(weeklyCalorieMission.get(i).getMission().getGoal())
+                    .build();
+
+            missionDetailDtoList.add(stepBuild);
+            missionDetailDtoList.add(calorieBuild);
+
+            MissionDto missionDto = MissionDto.builder()
+                    .title(weeklyStepMission.get(i).getMission().getTitle())
+                    .contents(weeklyStepMission.get(i).getMission().getContents() + weeklyCalorieMission.get(i).getMission().getContents())
+                    .designation(weeklyStepMission.get(i).getMission().getDesignation())
+                    .detail(missionDetailDtoList)
+                    .build();
+
+            missionDtoList.add(missionDto);
+        }
+
+        List<MissionDto> missionDtoList2 = getMissionDtoList(userMissions);
+        missionDtoList.addAll(missionDtoList2);
+
+        return missionDtoList;
     }
 
     public List<MissionDesignationDto> getUserDesignations(CustomUserDetails customUserDetails) {
