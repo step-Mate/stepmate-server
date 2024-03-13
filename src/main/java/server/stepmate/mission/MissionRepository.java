a47a8c70d53d5d398fe9a343a36cc14b3956aa66
apfhd12gk <apfhd12gk@naver.com>
2024-03-13 16:45:07 +0900

    feat: 미션칭호 이름으로 조회시 List를 통해 반환하도록 변경
    값이 1개가 아닌 경우가 있음 (ex:주간미션)

--- src/main/java/server/stepmate/mission/MissionRepository.java
+++ src/main/java/server/stepmate/mission/MissionRepository.java
@@ -3,8 +3,9 @@ package server.stepmate.mission;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.stepmate.mission.entity.Mission;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
-    Optional<Mission> findMissionByDesignation(String designation);
+    List<Mission> findMissionByDesignation(String designation);
 }
