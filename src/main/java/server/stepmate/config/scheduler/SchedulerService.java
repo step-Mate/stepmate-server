de0bc85fa485d19fdf57bb9dfeb65b67e0148500
apfhd12gk <apfhd12gk@naver.com>
2024-03-20 23:15:58 +0900

    refactor: 서버 모니터링을 위한 로그 추가

--- src/main/java/server/stepmate/config/scheduler/SchedulerService.java
+++ src/main/java/server/stepmate/config/scheduler/SchedulerService.java
@@ -1,12 +1,17 @@
 package server.stepmate.config.scheduler;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import server.stepmate.mission.MissionService;
 import server.stepmate.rank.RankService;
 import server.stepmate.user.UserService;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SchedulerService {
@@ -19,11 +24,13 @@ public class SchedulerService {
     public void initDay() {
         rankService.updateRank();
         userService.resetAllUserTodayStep();
+        log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + " : " + "initDay 실행");
     }
 
     @Scheduled(cron = "0 0 0 ? * MON") // 매주 월요일 자정
     public void initWeek() {
         missionService.resetWeeklyUserMission(); // 주간 미션 초기화
+        log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + " : " + "initWeek 실행");
     }
 
     @Scheduled(cron = "0 0 0 1 * ?") // 매월 1일 자정
@@ -31,6 +38,7 @@ public class SchedulerService {
         userService.resetAllDailyStep();
         userService.resetAllUserMonthStep(); // 월 걸음수 초기화
         missionService.resetMonthlyUserMission(); // 월간 미션 초기화
+        log.info(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss")) + " : " + "initMonth 실행");
     }
 
 }
